f10730c0559154a6e0589978dc7806694ae439bd
Larissa Pissurno <larissapissurno@gmail.com>
2025-06-19 22:43:47 -0300

    implement logout logic in the logout route to connect with backend session management

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,14 +1,6 @@
 import type { Route } from "./+types/logout";
+import { logout } from "~/.server/session";
 
 export async function action({ request }: Route.ActionArgs) {
-  // const { logout } = await import("~/utils/session.server");
-  // return logout(request);
-  return null;
-}
-
-export async function loader() {
-  // Redirect to login if someone tries to access /logout directly
-  // const { logout } = await import("~/utils/session.server");
-  // return logout(new Request(""));
-  return null;
+  return logout(request);
 }
