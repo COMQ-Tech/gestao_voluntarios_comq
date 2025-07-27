2df5be158aa07c11bb9d9fbf02ca8c6b071ca00f
Larissa Pissurno <larissapissurno@gmail.com>
2025-07-26 23:27:06 -0300

    fix: update import path for logout session to use absolute path

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/logout";
-import { logout } from "~/.server/session";
+import { logout } from "@/.server/session";
 
 export async function action({ request }: Route.ActionArgs) {
   return logout(request);
